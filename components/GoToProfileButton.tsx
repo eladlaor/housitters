da9b52197dcc3b4aceb4eac4a10a7a5268e64789
Elad Laor <eladlaor88@gmail.com>
2022-11-14 18:10:32 +0200

    go to profile button

---
+++ components/GoToProfileButton.tsx
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+export default function GoToProfileButton() {
+    return (
+        <div>
+          
+        </div>
+    )
+}
+
+
+
---
+++ utils/constants.ts
@@ -0,0 +1,14 @@
+
+export const USER_TYPE = {
+    HouseOwner:'houseowner',
+    Housitter: 'housitter',
+    None: 'none'
+  }
+
+export const HOUSITTERS_ROUTES = {
+    HOME: '/housitters/Home'
+}
+
+export const HOUSEOWNERS_ROUTES = {
+    HOME: '/house-owners/Home'
+}
