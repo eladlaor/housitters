3b4b75c06d26c97651156e53cfe465267601fae7
Elad Laor <eladlaor88@gmail.com>
2022-11-15 15:17:04 +0200

    basic implementation of the gotoprofile component

--- components/GoToProfileButton.tsx
+++ components/GoToProfileButton.tsx
@@ -1,13 +1,12 @@
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
-export default function GoToProfileButton() {
-    return (
-        <div>
-          
-        </div>
-    )
+export default function GoToProfileButton(props: any) {
+  return (
+    <div>
+      <button>
+        <Link href={`${props.baseRoute}?firstName=${props.firstName}`}>go to account</Link>
+      </button>
+    </div>
+  )
 }
-
-
-
--- pages/housitters/Home.tsx
+++ pages/housitters/Home.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import GoToProfileButton from '../../components/GoToProfileButton'
 
 export default function Home() {
   const router = useRouter()
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <div>
       <h1>Hello {firstName}! Let's find you a cute pet to feel at home with.</h1>
+      <GoToProfileButton baseRoute="/housitters/HousitterAccount" firstName={firstName}/>
     </div>
   )
 }
