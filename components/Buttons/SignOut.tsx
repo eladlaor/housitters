90b25d97c38c729a659b5f43e536abc897778e65
Elad Laor <eladlaor88@gmail.com>
2023-03-05 18:16:21 +0200

    signout is now implemented better, more DRY and mainly clears housitter or landlord states as well, not just user

--- components/Buttons/SignOut.tsx
+++ components/Buttons/SignOut.tsx
@@ -1,31 +1,44 @@
 import { useSessionContext } from '@supabase/auth-helpers-react'
 import Link from 'next/link'
 
-import { settersToInitialStates } from '../../slices/userSlice'
-import { useDispatch } from 'react-redux'
+import {
+  settersToInitialStates as userSettersToInitialStates,
+  SettersToInitialStates,
+  selectPrimaryUseState,
+} from '../../slices/userSlice'
+import { settersToInitialStates as housitterSettersToInitialStates } from '../../slices/housitterSlice'
+import { settersToInitialStates as landlordSettersToInitialStates } from '../../slices/landlordSlice'
+import { useSelector, useDispatch } from 'react-redux'
 import { useRouter } from 'next/router'
 
 import { userLogout } from '../../utils/auth/userLogout'
+import { USER_TYPE } from '../../utils/constants'
 
 export default function SignOut() {
   const { supabaseClient } = useSessionContext()
   const router = useRouter()
   const dispatch = useDispatch()
 
+  const userType = useSelector(selectPrimaryUseState)
+
+  const clearState = async (settersToInitialState: SettersToInitialStates) => {
+    settersToInitialState.forEach((attributeSetterAndInitialState) => {
+      dispatch(
+        attributeSetterAndInitialState.matchingSetter(attributeSetterAndInitialState.initialState)
+      )
+    })
+  }
+
   return (
     <button
       onClick={async () => {
-        const clearUserState = async () => {
-          settersToInitialStates.forEach((attributeSetterAndInitialState) => {
-            dispatch(
-              attributeSetterAndInitialState.matchingSetter(
-                attributeSetterAndInitialState.initialState
-              )
-            )
-          })
+        debugger
+        await clearState(userSettersToInitialStates)
+        if (userType === USER_TYPE.Housitter) {
+          await clearState(housitterSettersToInitialStates)
+        } else {
+          await clearState(landlordSettersToInitialStates)
         }
-
-        await clearUserState()
         await supabaseClient.auth.signOut()
         router.push('/')
       }}
