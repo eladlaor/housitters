b9f7d286956e81823e2ad6c5ba310425f1deab63
Elad Laor <eladlaor88@gmail.com>
2023-05-18 22:17:16 +0300

    a script to remove all files

---
+++ scripts/deleteAllFilesInBucket.ts
@@ -0,0 +1,36 @@
+const { createClient } = require('@supabase/supabase-js')
+
+const supabase = createClient(
+  'https://rssznetfvuqctnxfwvzr.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzc3puZXRmdnVxY3RueGZ3dnpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgyNjAyODksImV4cCI6MTk4MzgzNjI4OX0.fyWaISimfSUGQoavOyLah6loAkm3LwJl_YfFDspauIg'
+)
+
+const bucketName = 'posts'
+
+async function deleteAllFilesInBucket() {
+  console.log(`removing all files from: ${bucketName} `)
+
+  // Retrieve all files in the storage bucket
+  const { data, error } = await supabase.storage.from(bucketName).list()
+  console.log(`data is: ${JSON.stringify(data)}`)
+
+  if (error) {
+    console.error('Error retrieving files:', error)
+    return
+  }
+
+  // Delete each file in the bucket
+  for (const file of data) {
+    const { data, error } = await supabase.storage.from(bucketName).remove(file.name)
+
+    if (error) {
+      console.log('Error removing file: ', error)
+    } else {
+      console.log(`Deleted file: ${file.name}`)
+    }
+  }
+
+  console.log('All files deleted.')
+}
+
+deleteAllFilesInBucket()
---
+++ utils/files.ts
@@ -0,0 +1,98 @@
+import Resizer from 'react-image-file-resizer'
+
+export function removeInvalidCharacters(fileName: string): string {
+  const hebrewToEnglishMap: { [key: string]: string } = {
+    א: 'a',
+    ב: 'b',
+    ג: 'g',
+    ד: 'd',
+    ה: 'h',
+    ו: 'v',
+    ז: 'z',
+    ח: 'kh',
+    ט: 't',
+    י: 'y',
+    כ: 'k',
+    ל: 'l',
+    מ: 'm',
+    נ: 'n',
+    ס: 's',
+    ע: 'a',
+    פ: 'p',
+    צ: 'ts',
+    ק: 'k',
+    ר: 'r',
+    ש: 'sh',
+    ת: 't',
+    ן: 'n',
+    ך: 'k',
+    ם: 'm',
+    ף: 'p',
+    ץ: 'ts',
+    '׳': "'",
+    '״': '"',
+  }
+
+  const hebToEngRegex = new RegExp(Object.keys(hebrewToEnglishMap).join('|'), 'g')
+  const noHebrewFileName = fileName.replace(hebToEngRegex, (match) => hebrewToEnglishMap[match])
+
+  const allInvalidFileNameCharacters = /[^a-zA-Z0-9]/g
+  return noHebrewFileName.replace(allInvalidFileNameCharacters, '')
+}
+
+export const resizeImage = (file: File, maxWidth: number, maxHeight: number): Promise<Blob> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = (event) => {
+      const img = new window.Image()
+      img.src = reader.result as string
+      img.onload = () => {
+        const aspectRatio = img.width / img.height
+        let targetWidth, targetHeight
+
+        if (aspectRatio < 1) {
+          // horizontal
+          targetWidth = maxWidth
+          targetHeight = maxWidth / aspectRatio
+        } else {
+          // vertical
+          targetWidth = maxHeight * aspectRatio
+          targetHeight = maxHeight
+        }
+
+        Resizer.imageFileResizer(
+          file,
+          targetWidth,
+          targetHeight,
+          'JPEG',
+          80,
+          0,
+          (resizedImage: any) => {
+            resolve(resizedImage)
+          },
+          'blob'
+        )
+      }
+    }
+  })
+}
+
+// TODO: get rid of the resolve reject syntax
+export function blobToBuffer(blob: Blob): Promise<Buffer> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsArrayBuffer(blob)
+    reader.onload = () => {
+      if (reader.result instanceof ArrayBuffer) {
+        const buffer = Buffer.from(reader.result)
+        resolve(buffer)
+      } else {
+        reject(new Error('Failed to convert Blob to Buffer.'))
+      }
+    }
+    reader.onerror = (error) => {
+      reject(error)
+    }
+  })
+}
